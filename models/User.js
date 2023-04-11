f13137df384835011c9a4c88130ae77f3831daaa
teddy <baloukaulrich64@gmail.com>
2023-04-11 02:44:32 +0200

    schema

---
+++ models/User.js
@@ -0,0 +1,83 @@
+import mongoose from 'mongoose';
+const { ObjectId } = mongoose.Schema;
+
+ const userSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required:"Please enter you name"
+    },
+    email:{
+        type: String,
+        required : "Please enter your email",
+        trim: true,
+        unique: true,
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    role:{
+        type: String,
+        default: "user",
+    },
+    image:{
+        type: String,
+        default:"https://cdn-icons-png.flaticon.com/512/149/149071.png",
+    },
+    emailVerified:{
+        type: Boolean,
+        default: false,
+    },
+    defaultPaymentMethod:{
+        type: String,
+        default: ""
+    },
+    address:[{
+        firstName:{
+            type: String,
+        },
+        lastName:{
+            type: String,
+        },
+        phoneNumber1:{
+            type: String,
+        },
+        phoneNumber2:{
+            type: String,
+        },
+        address1:{
+            type: String,
+        },
+        city:{
+            type: String,
+        },
+        state:{
+            type: String,
+        },
+        country:{
+            type: String,
+        },
+        active:{
+            type: Boolean,
+            default: false,
+        },
+
+    }],
+    wishlist: [
+        {
+          product: {
+            type: ObjectId,
+            ref: "Product",
+          },
+          style: {
+            type: String,
+          },
+        },
+      ],
+ },
+ {
+    timestamps: true,
+ }
+ );
+ const User = mongoose.models.User || mongoose.model('User', userSchema);
+ export default User;
